c5fad523408802ec321c5ba4ea8cbce3edf73bfa
Sarad Vishwakarma <infosharadv@gmail.com>
2020-04-04 20:38:37 +0530

    add modified files

---
+++ src/app/text-transformation.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TextTransformationDirective } from './text-transformation.directive';
+
+describe('TextTransformationDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TextTransformationDirective();
+    expect(directive).toBeTruthy();
+  });
+});
