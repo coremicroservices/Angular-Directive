1b041c3a2960dda3c707804d168f4e2d7c8a34c4
Sarad Vishwakarma <infosharadv@gmail.com>
2020-04-04 21:57:56 +0530

    Directive  completed

--- src/app/text-transformation.directive.ts
+++ src/app/text-transformation.directive.ts
@@ -1,10 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appTextTransformation]'
 })
 export class TextTransformationDirective {
 
-  constructor() { }
+  // tslint:disable-next-line: no-input-rename
+  fontSize: string;
+  @HostBinding('style.background-color') background: string;
+
+  constructor(private elementRef: ElementRef) {
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.background = 'navajowhite';
+    this.fontSize = '50px';
+    this.IncreaseFontSize();
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.background = 'yellow';
+    this.fontSize = '12px';
+    this.IncreaseFontSize();
+  }
+
+  private IncreaseFontSize() {
+    this.elementRef.nativeElement.style.fontSize = this.fontSize;
+  }
 
 }
